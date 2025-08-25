2244b740da4fb1787b0c6688d34f7d5540854085
RafaelS0 <rafaeloliva2002@outlook.com>
2025-08-25 01:15:46 -0300

    refactor:  marcação nos testes de cada operação avançada

--- Documentos/Semana 9 - Base de versionamento e programação em Python/CalculadoraPython/tests/test_operacoes_avancadas.py
+++ Documentos/Semana 9 - Base de versionamento e programação em Python/CalculadoraPython/tests/test_operacoes_avancadas.py
@@ -1,6 +1,7 @@
 import calculadora
 import pytest
 
+@pytest.mark.fatorial
 def test_fatorial():
     calc = calculadora.CalculadoraPython()
     assert calc.fatorial(1) == 1
@@ -8,11 +9,11 @@ def test_fatorial():
     assert calc.fatorial(3) == 6
     assert calc.fatorial(4) == 24
     assert calc.fatorial(5) == 120
-
+@pytest.mark.fatorial
 def test_fatorial_de_zero():
     calc = calculadora.CalculadoraPython()
     assert calc.fatorial(0) == 1
-
+@pytest.mark.potencia
 def test_potencia():
     calc = calculadora.CalculadoraPython()
     assert calc.potencia(2, 3) == 8
@@ -20,12 +21,12 @@ def test_potencia():
     assert calc.potencia(5, 0) == 1
     assert calc.potencia(0, 5) == 0
     assert calc.potencia(2, -1) == 0.5
-
+@pytest.mark.logaritmo
 def test_logaritmo_natural():
     calc = calculadora.CalculadoraPython()
     assert calc.log_natural(3) == pytest.approx(1.0986, 0.0001)
     assert calc.log_natural(1) == 0
-
+@pytest.mark.logaritmo
 def test_logaritmo_natural_negativo():
     calc = calculadora.CalculadoraPython()
     try:
