745248f70715e722c56fae39b89c14e89dbaf621
Rafael Soares <rafaeloliva2002@outlook.com>
2025-10-03 09:45:34 -0300

    refactor: organização das funções  de chamadas no banco de dados

--- Documentos/Semana 15 - Challenge Final/cinemaApp-tests/api/support/libs/database.py
+++ Documentos/Semana 15 - Challenge Final/cinemaApp-tests/api/support/libs/database.py
@@ -6,7 +6,57 @@ client = MongoClient('mongodb://localhost:27017/')
 
 db = client['cinema-app']
 
-#Aprimorado com IA
+###USER TABLE###
+
+@keyword('Insert user into database')
+def insert_user(user_data):
+    users = db['users']
+    
+    existing_user = users.find_one({'email': user_data['email']})
+    if existing_user:
+        print(f"User with email {user_data['email']} already exists")
+    else:
+        users.insert_one(user_data)
+        print(f"User with email {user_data['email']} inserted successfully")
+
+@keyword('Clean user from database')
+def remove_user(email):
+   users = db['users']
+   users.delete_many({'email': email})
+   print(f"Removed user with email: {email}")
+
+###TICKET TABLE###
+
+@keyword('Insert ticket into database')
+def insert_ticket(ticket_data):
+    tickets = db['tickets']
+    tickets.insert_one(ticket_data)
+
+#IA
+@keyword('Get Theater by Name')
+def get_theater_by_name(name):
+    theaters = db['theaters']
+    theater = theaters.find_one({'name': name})
+    if theater and '_id' in theater:
+        theater['_id'] = str(theater['_id'])
+    return theater
+
+###MOVIE TABLE###
+
+@keyword('Insert movie into database')
+def insert_movie(movie_data):
+    movies = db['movies']
+    movies.insert_one(movie_data)
+
+@keyword('Get movie by title')
+def get_movie_by_title(title):
+    movies = db['movies']
+    movie = movies.find_one({'title': title})
+    if movie and '_id' in movie:
+        movie['_id'] = str(movie['_id'])
+    return movie
+
+#IA
 @keyword('Clean Movie from Database')
 def clean_movie(title):
     movies = db['movies']
@@ -24,38 +74,21 @@ def clean_movie(title):
         all_movies = list(movies.find({}, {'title': 1}))
         print(f"Existing movies: {all_movies}")
 
-@keyword('Clean user from database')
-def remove_user(email):
-   users = db['users']
-   users.delete_many({'email': email})
-   print(f"Removed user with email: {email}")
 
-@keyword('Insert movie into database')
-def insert_movie(movie_data):
-    movies = db['movies']
-    movies.insert_one(movie_data)
+###THEATER TABLE###
 
-@keyword('Get movie by title')
-def get_movie_by_title(title):
-    movies = db['movies']
-    movie = movies.find_one({'title': title})
-    if movie and '_id' in movie:
-        movie['_id'] = str(movie['_id'])
-    return movie
+@keyword('Insert theater into database')
+def insert_theater(theater_data):
+    theaters = db['theaters']
+    theaters.insert_one(theater_data)
 
 @keyword('Remove Theater From Database')
 def remove_theater_by_name(name):
     theaters = db['theaters']
     theaters.delete_many({'name': name})
 
-#IA
-@keyword('Get Theater by Name')
-def get_theater_by_name(name):
-    theaters = db['theaters']
-    theater = theaters.find_one({'name': name})
-    if theater and '_id' in theater:
-        theater['_id'] = str(theater['_id'])
-    return theater
+
+###SESSION TABLE###
 #IA
 @keyword('Get Session From Database')
 def get_session_by_date(datetime):
@@ -97,15 +130,3 @@ def get_session_by_theater(theater_id):
 def remove_session_by_date(date):
     sessions = db['sessions']
     sessions.delete_many({'date': date})
-
-@keyword('Insert user into database')
-def insert_user(user_data):
-    users = db['users']
-    
-    existing_user = users.find_one({'email': user_data['email']})
-    if existing_user:
-        print(f"User with email {user_data['email']} already exists")
-    else:
-        users.insert_one(user_data)
-        print(f"User with email {user_data['email']} inserted successfully")
-
